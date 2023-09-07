fd27715d55e58057a036ddf379a2958727e100c6
siriuzz <elianmatosd03@gmail.com>
2023-09-07 01:50:14 -0400

    Cambios al metro.config.js, resolver errores de android build

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
