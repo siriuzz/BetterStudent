b99bf49015dfe963263a2f180da3e4d0d7332af8
siriuzz <elianmatosd03@gmail.com>
2023-09-07 00:19:39 -0400

    Firebase añadido al proyecto

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7n-_rxgpAp_r5t87blrMLnX8R1tfi9i8",
+  authDomain: "betterstudent-2d8de.firebaseapp.com",
+  projectId: "betterstudent-2d8de",
+  storageBucket: "betterstudent-2d8de.appspot.com",
+  messagingSenderId: "102026675691",
+  appId: "1:102026675691:web:08e6fd642dd9a93292c5ae",
+  measurementId: "G-9Y0HC7WVP6"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function App() {
   return (
