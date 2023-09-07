71e7ee3797300481600b146a54daa943ee8f1f3a
siriuzz <elianmatosd03@gmail.com>
2023-09-07 09:25:55 -0400

    variables de entorno creadas, se debe hacer revert a commit anterior

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,5 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'), // Customize asset extensions as needed
-      sourceExts,
-    },
-  };
-})();
+module.exports = config;
