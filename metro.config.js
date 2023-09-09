053234f1a634221c063bdecf5d5ad80154e6f5e6
siriuzz <elianmatosd03@gmail.com>
2023-09-08 23:43:36 -0400

    Pruebas unitarias a firestore y lint

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,9 @@
+/* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(
+   __dirname 
+);
 
 module.exports = config;
