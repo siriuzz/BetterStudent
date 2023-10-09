f2c852212e443ea5bddf353e77bfb5d19b9beca9
siriuzz <elianmatosd03+github@gmail.com>
2023-10-09 03:21:04 -0400

    firestore instance added to test

--- tests/firestore.test.js
+++ tests/firestore.test.js
@@ -1,19 +1,6 @@
-const { initializeApp } = require('firebase/app');
+const controllers = require('../firebase/controllers');
+const db = require('../firebase/firestore');
 const { getFirestore, collection, addDoc, doc, getDoc, deleteDoc, setDoc } = require('firebase/firestore');
-require('dotenv').config();
-
-const firebaseConfig = {
-    apiKey: process.env.EXPO_PUBLIC_API_KEY,
-    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.EXPO_PUBLIC_APP_ID,
-    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
 
 // Define a collection and document for testing
 const testCollection = 'comments';
