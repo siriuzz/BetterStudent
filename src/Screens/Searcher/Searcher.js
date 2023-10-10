a9fc68a8efd483e931815a0155ea1e4a0304becd
leonelSG <1104995@est.intec.edu.do>
2023-10-10 02:45:54 -0400

    crear barra de busqueda

--- src/Screens/Searcher/Searcher.js
+++ src/Screens/Searcher/Searcher.js
@@ -1,5 +1,7 @@
+import { color } from "@mui/system";
 import { settings } from "firebase/analytics";
 import React from "react";
+import { useState } from "react";
 import {
   View,
   Text,
@@ -8,13 +10,72 @@ import {
   Alert,
   Keyboard,
   Image,
+  StyleSheet,
+  TouchableOpacity,
 } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { Searchbar } from 'react-native-paper';
 
-export default function SearchScreen({navigation}){
-  return(
-    <View>
-      <Text>Search Screen</Text>
-    </View>
-  )
-}
 
+
+const handleSearch = (query) => {
+  // para la logica de busqueda
+  
+};
+
+const handleClearHistory = () => {
+  // para limpiar el historial
+  setSearchHistory([]);
+};
+
+
+const Sbar = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchHistory, setSearchHistory] = useState([]);
+  return (
+    <SafeAreaView style={styles.container}>
+        <Searchbar
+        placeholder="Buscar Estudiante"
+        value={searchQuery}
+        iconColor="#C08708"
+        placeholderTextColor={"#C08708"}
+        style={styles.searchBar}
+        inputStyle={styles.searchInput}
+        onChangeText={(query) => setSearchQuery(query)}
+        onIconPress={() => handleSearch(searchQuery)}
+    />
+    
+        {searchHistory.length > 0 && (
+        <TouchableOpacity onPress={handleClearHistory} style={styles.clearButton}>
+          <Ionicons name="ios-trash-outline" size={24} color="red" />     
+        </TouchableOpacity>
+      )}
+    </SafeAreaView>
+    //el icono que esta con el searchHistory solo debe aparecer cuando tenga un historial
+  );
+};
+
+export default Sbar;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 50,
+    
+  },
+  searchBar: {
+    width: '95%', 
+    //height: '10%',
+    backgroundColor: '#efefef',
+    borderWidth: 1, 
+    borderRadius: 50, 
+    borderColor: '#001347', 
+    paddingHorizontal: 16,
+  
+  },
+  searchInput: {
+    fontSize: 16,
+    
+  },
+});
